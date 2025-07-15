45e15ed6873a55334ed7976177062235d3c4c1c3
JSQdev <148073798+devJSQ@users.noreply.github.com>
2025-07-15 05:13:00 +0300

    Update Order.java

--- src/Order.java
+++ src/Order.java
@@ -1,14 +1,24 @@
-class Order {
-
+import java.util.*;
+public class Order {
+    public static int nextId = 1;
     public int ID;
     public double totalPrise;
-    /* Add your code here 
-     * Collection of items in a container
-    */
-    
-    
-    /* Add your code here 
-     * Constructor
-    */
+    public List<Item> items;
 
+    public Order() {
+        this.ID = nextId++;
+        this.items = new ArrayList<>();
+        this.totalPrise = 0.0;
+    }
+    public void addItem(Item item) {
+        items.add(item);
+        totalPrise += item.price;
+    }
+    public void printOrder() {
+        System.out.println("Order ID: " + ID);
+        for (Item item : items) {
+            System.out.printf("%s %.2f\n", item.name, item.price);
+        }
+        System.out.printf("Total: %.2f\n", totalPrise);
+    }
 }
